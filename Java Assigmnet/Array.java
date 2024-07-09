08bb194e6a76e0a90a8ea789943ffb894083ea71
Sachidanand Pandey <168963847+sachidanand9@users.noreply.github.com>
2024-07-09 11:46:47 +0530

    Add files via upload

---
+++ Java Assigmnet/Array.java
@@ -0,0 +1,16 @@
+public class Array {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        int min = arr[0];
+        int max = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            } else {
+                max = arr[i];
+            }
+        }
+        System.out.println("Min : " + min + ", Max : " + max);
+    }
+}
