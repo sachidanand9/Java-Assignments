cf9c77f10b6f1ddb494c190d17e52154374783e8
Sachidanand Pandey <168963847+sachidanand9@users.noreply.github.com>
2024-09-05 22:03:58 +0530

    first commit

---
+++ Array Problems/Descending_order.java
@@ -0,0 +1,20 @@
+public class Descending_order {
+    public static void main(String[] args) {
+        int arr[] = {2,3,1,45,23,46,87,32};
+        int temp = 0; // tempary variable to store the values 
+
+        for(int i = 0; i<arr.length;i++){
+          for (int j = i+1;j<arr.length;j++){
+            if (arr[i]<arr[j]) {
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+          }
+        }
+        System.out.println("Element in Decending Order :");
+        for(int i = 0; i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+    }
+}
+}
