8250b5a85cf606b094a5494870a26ea960071b62
Sachidanand Pandey <168963847+sachidanand9@users.noreply.github.com>
2025-01-15 22:38:11 +0530

    first commit

---
+++ 2025/Bitwise_opertor.java
@@ -0,0 +1,11 @@
+public class Bitwise_opertor {
+    public static void main(String[] args) {
+        int p = 9, q = 10;
+
+        System.out.println(p|q);
+        System.out.println(p&q);
+        System.out.println(p^q);
+        System.out.println(p<<2);
+        System.out.println(p>>3);
+    }
+}
