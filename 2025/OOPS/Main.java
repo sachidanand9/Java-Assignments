269953453c4b3e0809b86bd01750026ee4c73bf5
Sachidanand Pandey <168963847+sachidanand9@users.noreply.github.com>
2025-01-15 22:38:40 +0530

    first commit

---
+++ 2025/OOPS/Main.java
@@ -0,0 +1,13 @@
+class Student{
+    int rollno;
+    String name;
+}
+public class Main {
+    public static void main(String[] args) {
+     Student s = new Student();
+     s.rollno = 10;
+     s.name = "Ayush";
+     System.out.println(s.rollno);
+     System.out.println(s.name);
+    }
+}
